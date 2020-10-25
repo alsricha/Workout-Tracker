84cc67a602fd1174c2834d6bef67e677a5d64f38
Alex Skudrovskis <alex.skudrovskis@gmail.com>
2020-10-25 11:03:11 -0600

    Debugging

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const Workout  = require("../models/workout.js");
+const db = require("../models");
 
 // Gets all previous workouts
 router.get('/api/workouts', (req, res) => {
