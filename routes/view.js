07ccb9488a89bc3c27d716222895986e86641a03
Alex Skudrovskis <alex.skudrovskis@gmail.com>
2020-10-25 10:43:56 -0600

    added an index.html route

--- routes/view.js
+++ routes/view.js
@@ -1,6 +1,10 @@
 const path = require("path");
 const router = require("express").Router();
 
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
 router.get("/excerise", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/excerise.html"));
 });
